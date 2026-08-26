0ea813ae9ca3b296c1006522e3bd8c0dd9b00851
nappynapp <joanneje05@gmail.com>
2026-08-26 14:42:55 -0500

    bfgasgiluhef

--- home.js
+++ home.js
@@ -1,1 +1,19 @@
 console.log ("Hello world :D");
+
+function setup() {
+    createCanvas(400,400)
+}
+
+function draw() {
+    background(200)
+    rectMode (CENTER)
+    rect(mouseX, mouseY,50,50)
+}
+
+function windowResize() {
+    background (255,0,0)
+    resizeCanvas
+    (windowWidth,
+    windowHeight
+    )
+}
